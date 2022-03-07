b90c0833dcc28907f59be5ef652cc66eb3a6044f
Andre-Oberg <andre.oberg@ha.ax>
2022-03-07 17:50:28 +0200

    Initial commit

---
+++ src/main/java/ax/ha/it/marsrover/Movement.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ax.ha.it.marsrover;
+
+/**
+ *
+ * @author André
+ */
+public class Movement {
+    
+    private int x = 0;
+    
+    private int y = 0;
+    
+    private char direction; 
+    
+    public Int getCoordinates () {
+        
+    }
+    
+    public void initialPosition(int xPos, int yPos, char dir) {
+        
+    }
+ 
+    public int moveForward() {
+        return 0;
+    }
+           
+    public int moveBackward() {
+        return 0;
+    }
+    
+    public int turnLeft() {
+        return 0;
+    }
+    
+    public int turnRight() {
+        return 0;
+    }
+    
+    public boolean validatePosition() {
+        return true;
+    }
+    
+}
